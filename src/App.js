c18e061f0df9d6e3889e091d56811d3237caa2ab
John Li <john.lizuhua@outlook.com>
2021-09-23 23:31:20 -0400

    chat engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {ChatEngine} from "react-chat-engine";
+import "./App.css";
+
+const App = () =>{
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="
+c9def413-61d1-4c2d-9efc-c9dfadf351e2"
+            userName="johnny"
+            userSecret="111111"
+        />
+    );
+}
+
+export default App;
